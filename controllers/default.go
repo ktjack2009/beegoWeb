b4def800bce2b7afa1f205bdf2f38b6dfae8a523
ktjack2009 <736738213@qq.com>
2019-03-17 20:07:45 +0800

    配置文件

--- controllers/default.go
+++ controllers/default.go
@@ -5,11 +5,11 @@ import (
 )
 
 type MainController struct {
-	beego.Controller
+	beego.Controller // controller实现beego.Controller自带的所有方法
 }
 
-func (c *MainController) Get() {
+func (c *MainController) Get() { // 改写Get方法
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	c.TplName = "index.tpl" // 指定渲染的模版
 }
--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,11 @@ package routers
 
 import (
 	"beegoWeb/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{}) // 将路由映射到controller
+	beego.Router("/index", &controllers.IndexController{})
 }
